8b16fb9a230f6c04cbba2dcac2cc1770b6d058c5
Ben Meller <ben.meller3@outlook.com>
2021-11-15 23:29:11 +1100

    Move playground to separate folder

---
+++ swift-cli/Sources/Playgrounds/dronePlayground.swift
@@ -0,0 +1,53 @@
+import Foundation
+import AudioKit
+import SoundpipeAudioKit
+
+class Drone {
+    let engine = AudioEngine()
+    
+    var osc1 = Oscillator()
+    var osc2 = Oscillator()
+    var mixer = Mixer()
+    
+    var _freq: AUValue = 440.0
+    var frequency: AUValue {
+        get {
+            return _freq
+        }
+        set(newFreq) {
+            _freq = newFreq
+            osc1.frequency = newFreq as AUValue
+            osc2.frequency = (newFreq * 3/4) as AUValue
+        }
+    }
+    
+    init() {
+        frequency = 440.0
+        mixer = Mixer(osc1, osc2)
+        mixer.volume = 0.7
+        engine.output = mixer
+    }
+    
+    func start() {
+        // Get key to plain drone in
+        // TODO
+        
+        // Set osc frequencies
+        osc1.frequency = frequency as AUValue
+        osc2.frequency = (frequency * 3/4) as AUValue  // 4th below osc1
+        
+        do {
+            try engine.start()
+        } catch let err {
+            Log(err)
+        }
+        osc1.start()
+        osc2.start()
+    }
+    
+    func stop() {
+        osc1.stop()
+        osc2.stop()
+        engine.stop()
+    }
+}
--- swift-cli/Sources/swift-cli/main.swift
+++ swift-cli/Sources/swift-cli/main.swift
@@ -3,61 +3,12 @@ import AudioKit
 import SoundpipeAudioKit
 
 print("Hello, world!")
-// Todo: Map key to frequency
 
-class Drone {
-    let engine = AudioEngine()
-    
-    var osc1 = Oscillator()
-    var osc2 = Oscillator()
-    var mixer = Mixer()
-    
-    var _freq: AUValue = 440.0
-    var frequency: AUValue {
-        get {
-            return _freq
-        }
-        set(newFreq) {
-            _freq = newFreq
-            osc1.frequency = newFreq as AUValue
-            osc2.frequency = (newFreq * 3/4) as AUValue
-        }
-    }
-    
-    init() {
-        frequency = 440.0
-        mixer = Mixer(osc1, osc2)
-        mixer.volume = 0.7
-        engine.output = mixer
-    }
-    
-    func start() {
-        // Get key to plain drone in
-        // TODO
-        
-        // Set osc frequencies
-        osc1.frequency = frequency as AUValue
-        osc2.frequency = (frequency * 3/4) as AUValue  // 4th below osc1
-        
-        do {
-            try engine.start()
-        } catch let err {
-            Log(err)
-        }
-        osc1.start()
-        osc2.start()
-    }
-    
-    func stop() {
-        osc1.stop()
-        osc2.stop()
-        engine.stop()
-    }
-}
-
-var drone = Drone()
+var drone = DroneContext(polyphony: 3)
 drone.start()
 while true {
+    drone.noteOn(note: 64 as MIDINoteNumber)
     usleep(2000000)
-    drone.frequency = drone.frequency * 1.12
+    drone.noteOff()
+    usleep(2000000)
 }
